43d7de43e58d6da3f7fc1e76bf1b64d89e8fbd6b
djedji <ctelyon@orange.fr>
2014-05-01 21:05:02 +0200

    Add DebugKit

--- base-2.4.7/app/Config/database.php
+++ base-2.4.7/app/Config/database.php
@@ -67,7 +67,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'nothing',
+		'database' => 'test',
 		'prefix' => '',
 		'encoding' => 'utf8'
 	);
